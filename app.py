161aed3950f4ab4081a2f8e047b9cc7f7c6c6943
Sakshi Shah <202201281@daiict.ac.in>
2026-01-04 17:25:35 +0530

    Refactor movie recommendation system with error handling
    Updated the movie recommendation system to include error handling for poster fetching, added data loading functionality with caching, and improved code structure with path setup.

--- app.py
+++ app.py
@@ -1,60 +1,79 @@
+import os
 import streamlit as st
 import pickle
 import pandas as pd
-
+import gdown
 import requests
 
+# ------------------ PATH SETUP ------------------
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MOVIE_DICT_PATH = os.path.join(BASE_DIR, "movie_dict.pkl")
+SIMILARITY_FILE = os.path.join(BASE_DIR, "similarity.pkl")
+
+SIMILARITY_URL = "https://drive.google.com/file/d/1MHYUMaeXQWdWx7YxUtA3V_B0dGlqxaHO/view?usp=sharing"
+
+# ------------------ FUNCTIONS ------------------
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+    try:
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+        data = requests.get(url, timeout=10).json()
+        poster_path = data.get("poster_path")
+        if poster_path:
+            return "https://image.tmdb.org/t/p/w500/" + poster_path
+    except:
+        pass
+    return "https://via.placeholder.com/300x450?text=No+Image"
+
 
 def recommend(movie):
-    movie_idx = movies[movies['title']== movie].index[0] # calculated the index of the movie
-    similarity_array =similarity[movie_idx]
+    movie_idx = movies[movies['title'] == movie].index[0]
+    similarity_array = similarity[movie_idx]
 
-    movies_list = sorted(list(enumerate(similarity_array)),reverse=True,key=lambda x:x[1])[1:6]
-    # sorted the similarity_array of the particular movie in decending order on the basis of score and made tupple of i9ndex and score to identify the movie
+    movies_list = sorted(
+        list(enumerate(similarity_array)),
+        reverse=True,
+        key=lambda x: x[1]
+    )[1:6]
 
-    recommended_movies =[]
-    recommended_movies_poster=[]
+    recommended_movies = []
+    recommended_movies_poster = []
+
     for i in movies_list:
-        movie_id =movies.iloc[i[0]].movie_id
-        #fetch poster
+        movie_id = movies.iloc[i[0]].movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
         recommended_movies_poster.append(fetch_poster(movie_id))
 
-    return recommended_movies,recommended_movies_poster
-
-movies_dict= pickle.load(open('movie_dict.pkl','rb'))
-movies = pd.DataFrame(movies_dict)
-
-similarity = pickle.load(open('similarity.pkl','rb'))
-st.title("Movie Recommendation System")
+    return recommended_movies, recommended_movies_poster
 
 
-selected_movie_name= st.selectbox("Select the movie that you like, and I will recommend similar movies",movies['title'].values)
+# ------------------ LOAD DATA ------------------
+@st.cache_resource
+def load_data():
+    if not os.path.exists(SIMILARITY_FILE):
+        with st.spinner("Downloading similarity model..."):
+            gdown.download(SIMILARITY_URL, SIMILARITY_FILE, quiet=False)
+
+    movies_dict = pickle.load(open(MOVIE_DICT_PATH, "rb"))
+    similarity = pickle.load(open(SIMILARITY_FILE, "rb"))
+
+    return pd.DataFrame(movies_dict), similarity
+
+
+movies, similarity = load_data()
+
+# ------------------ UI ------------------
+st.title("🎬 Movie Recommendation System")
+
+selected_movie_name = st.selectbox(
+    "Select a movie you like:",
+    movies['title'].values
+)
 
 if st.button("Recommend"):
-    names,posters = recommend(selected_movie_name)
+    names, posters = recommend(selected_movie_name)
 
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(names[0])
-        st.image(posters[0])
-    with col2:
-        st.text(names[1])
-        st.image(posters[1])
-    with col3:
-        st.text(names[2])
-        st.image(posters[2])
-    with col4:
-        st.text(names[3])
-        st.image(posters[3])
-    with col5:
-        st.text(names[4])
-        st.image(posters[4])
-
+    cols = st.columns(5)
+    for i, col in enumerate(cols):
+        with col:
+            st.text(names[i])
+            st.image(posters[i])
